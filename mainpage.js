272feab1f11fdbc4d13598d1217f563cd7b1b0f9
blendedcoffee <zarahussain0209@icloud.com>
2025-04-04 15:32:09 +0100

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,55 @@
+pages["Main"] = (() => {
+  let font;
+  let words = ["Zara Hussain", "Zara Hussain", "Zara Hussain", "Zara Hussain"];
+  let colors = ['#FFF4B1', '#FFD6E8', '#C3E8BD', '#AFCBFF', '#F6C1B0', '#E6E6FA', '#FAF0D7'];
+  let rows = 10;
+  let cols;
+  let frameHold = 60;
+  let frameCounter = 0;
+
+  return {
+    preload() {
+      font = loadFont('assets/PixelifySans-VariableFont_wght.ttf');
+    },
+    setup() {
+      cols = words.length;
+      textFont(font);
+    },
+    draw() {
+      if (frameCounter < frameHold) {
+        frameCounter++;
+        return;
+      }
+      frameCounter = 0;
+
+      blendMode(MULTIPLY);
+      push();
+      translate(150 + .03 * width, .025 * height); // offset for menu
+      scale(0.8);
+
+      for (let h = 0; h < cols; h++) {
+        let myWord = words[h];
+        let myH = h % 2 === 0 ? 1.52 : 1.15;
+
+        for (let i = 0; i < rows; i++) {
+          let yOffset = height * (i + myH) / rows;
+          let textSizeVal = height / rows;
+
+          let points = font.textToPoints(myWord, width / 15 + width * h / cols, yOffset, textSizeVal, {
+            sampleFactor: 0.5,
+            simplifyThreshold: map(i, 0, rows - 1, 0.01, 3.5)
+          });
+
+          stroke(random(colors));
+          noFill();
+          beginShape();
+          for (let pt of points) {
+            vertex(pt.x, pt.y);
+          }
+          endShape();
+        }
+      }
+      pop();
+    }
+  };
+})();
