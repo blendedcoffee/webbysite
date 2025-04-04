dfcb62019d307fea19b728a5a273dc2405d6648a
blendedcoffee <zarahussain0209@icloud.com>
2025-04-04 16:14:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,66 +1,103 @@
 let currentPage = "Main";
 let pages = {};
-
-function preload() {
-  if (pages[currentPage]?.preload) {
-    pages[currentPage].preload();
-  }
-}
+let menuItems = ["Main", "Calendar", "Posts", "Media"];
+let mediaSubpages = ["Films & TV", "Books", "Music"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
   textSize(24);
-  textFont('sans-serif'); // fallback
-
-  if (pages[currentPage]?.setup) {
-    pages[currentPage].setup();
-  }
 }
 
 function draw() {
+  clear();
   background('#F8F6EE');
-  drawVerticalMenu();
 
-  if (pages[currentPage]?.draw) {
+  drawNav(); // Always draw nav
+
+  if (pages[currentPage] && pages[currentPage].draw) {
     pages[currentPage].draw();
   }
 }
 
-function mousePressed() {
-  if (mouseX < 150) {
-    let menuItems = Object.keys(pages);
-    let itemHeight = 50;
-    for (let i = 0; i < menuItems.length; i++) {
-      let y = i * itemHeight + 50;
-      if (mouseY > y - 25 && mouseY < y + 25) {
-        currentPage = menuItems[i];
-        if (pages[currentPage]?.setup) {
-          pages[currentPage].setup();
-        }
-        break;
-      }
-    }
-  }
-}
+function drawNav() {
+  let navHeight = 60;
+  let totalItems = menuItems.length + (currentPage === "Media" ? mediaSubpages.length : 0);
+  let itemWidth = width / totalItems;
 
-function drawVerticalMenu() {
-  let menuItems = Object.keys(pages);
-  textAlign(LEFT, CENTER);
-  textSize(20);
-  fill(0);
+  // Background bar
+  fill('#FFF4B1');
+  noStroke();
+  rect(0, 0, width, navHeight);
+
+  // Draw top-level menu items
   for (let i = 0; i < menuItems.length; i++) {
-    let x = 20;
-    let y = i * 50 + 50;
+    let label = menuItems[i];
+    let x = i * itemWidth + itemWidth / 2;
+    let y = navHeight / 2;
 
-    if (mouseX < 150 && mouseY > y - 25 && mouseY < y + 25) {
-      fill(200, 100, 50);
+    if (isMouseOver(x, y, itemWidth, navHeight)) {
+      fill('#FFD6E8');
+      if (mouseIsPressed) {
+        currentPage = label;
+      }
     } else {
       fill(0);
     }
 
-    text(menuItems[i], x, y);
+    text(label, x, y);
   }
+
+  // Draw subpages under "Media" if it's the current page
+  if (currentPage === "Media") {
+    for (let i = 0; i < mediaSubpages.length; i++) {
+      let label = mediaSubpages[i];
+      let x = (menuItems.length + i) * itemWidth + itemWidth / 2;
+      let y = navHeight / 2;
+
+      if (isMouseOver(x, y, itemWidth, navHeight)) {
+        fill('#F6C1B0');
+        if (mouseIsPressed) {
+          currentPage = label;
+        }
+      } else {
+        fill(0);
+      }
+
+      text(label, x, y);
+    }
+  }
+}
+
+
+  // Optional: show subpages when in Media
+  if (currentPage === "Media") {
+    for (let i = 0; i < mediaSubpages.length; i++) {
+      let label = mediaSubpages[i];
+      let x = xOffset + (menuItems.length + i) * spacing;
+      let y = 30;
+
+      if (isMouseOver(x, y, spacing, 40)) {
+        fill('#F6C1B0');
+        if (mouseIsPressed) {
+          currentPage = label;
+        }
+      } else {
+        fill(0);
+      }
+
+      push();
+      translate(x, y);
+      rotate(-HALF_PI);
+      text(label, 0, 0);
+      pop();
+    }
+  }
+}
+
+function isMouseOver(x, y, w, h) {
+  return mouseX > x - w / 2 && mouseX < x + w / 2 &&
+         mouseY > y - h / 2 && mouseY < y + h / 2;
 }
 
 function windowResized() {
